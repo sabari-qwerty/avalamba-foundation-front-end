1e689e8378498f401e136600cfed9748c6582486
sabari-qwerty <sabarigithub@gmail.com>
2023-05-23 10:52:18 +0530

    update

--- pages/api/createdb.ts
+++ pages/api/createdb.ts
@@ -7,41 +7,8 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === "POST") {
-        // console.log('hello')
         const data = req.body
         console.log(data)
-
-        // const {
-        //     name,
-        //     email,
-        //     pan_number,
-        //     phone_number,
-        //     amount,
-        //     currency,
-        //     razorpay_payment_id,
-        //     razorpay_order_id,
-        //     razorpay_signature_id,
-        //     template_header,
-        // } = data
-
-
-        // console.log(
-        //     {
-        //         name,
-        //         email,
-        //         pan_number,
-        //         phone_number,
-        //         amount,
-        //         currency,
-        //         razorpay_payment_id,
-        //         razorpay_order_id,
-        //         razorpay_signature_id,
-        //         template_header,
-        //     }
-        // )
-        // console.log(data)
-
-        // console.log(data)
         await prisma.donationDetails.create({
             data
         })
