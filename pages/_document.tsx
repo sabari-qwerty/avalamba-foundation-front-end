823b16d340996a1d100cd4c5a57f50bf168b83dd
viswa-gz <viswa@gozen.io>
2023-08-24 12:47:52 +0530

    update pop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
+      <Head>
         <title>Avalamb foundation</title>
-        <link rel='icon' type="image/x-icon" href='/logo/icon.ico' />
-
+        <link rel="icon" type="image/x-icon" href="/logo/icon.ico" />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Script src='https://checkout.razorpay.com/v1/checkout.js' />
+        <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-GVTRYKCK0M" />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+          gtag('config', 'G-GVTRYKCK0M');`}
+        </Script>
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { Objectives } from "@/components/HomePage/Objectives";
 import { Projects } from "@/components/HomePage/Projects";
 import { Articles_and_Blogs } from "@/components/HomePage/Articles_and_Blogs";
 import { FiexedButton } from "@/components/HomePage/FiexedButton";
+import Script from "next/script";
 // import { ImageSwper } from './HomePage/Swiper'
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,14 +21,13 @@ export default function Home() {
       {/* <Projects /> */}
       {/* <Articles_and_Blogs /> */}
       <FiexedButton text="Contribute Now" />
-
-      <iframe
-        width="500px"
-        height="500px"
-        src="https://optin.ly.gozen.io/link?campaign_id=b5666e56-4cf4-4cc5-ab82-6bc0b3064ff5&site_id=d06e0674-5bdc-4e7b-b85d-e6f578a44f84"
-      >
-        {" "}
-      </iframe>
+      <Script
+        id="optinly_script"
+        async={true}
+        data-app_id="d06e0674-5bdc-4e7b-b85d-e6f578a44f84"
+        src="https://cdn.dev.optinly.gozen.io/v1/optinly.js"
+        type="text/javascript"
+      />
     </Layout>
   );
 }
