4cdeb3c39b8c568b86eef53551b783357da28515
viswa-gz <viswa@gozen.io>
2023-09-05 18:01:58 +0530

    update script tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,14 +33,18 @@ export default function Home() {
           
           gtag('config', 'G-5QK6TSQ9EQ');
         `}
+        {/* <!-- Google Tag Manager --> */}
+
+        {/* <!-- End Google Tag Manager --> */}
+      </Script>
+
+      <script>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-TQQMNSWH');`}
-      </Script>
-      {/* <!-- Google Tag Manager --> */}
-      {/* <!-- End Google Tag Manager --> */}
+      </script>
     </Layout>
   );
 }
