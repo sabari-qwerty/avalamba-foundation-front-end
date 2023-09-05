94321d850034d00e0c2faa5642140467052bb660
viswa-gz <viswa@gozen.io>
2023-09-05 18:21:06 +0530

    update script id

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,30 +21,7 @@ export default function Home() {
       {/* <Projects /> */}
       {/* <Articles_and_Blogs /> */}
       <FiexedButton text="Contribute Now" />
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5QK6TSQ9EQ"
-      />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          
-          gtag('config', 'G-5QK6TSQ9EQ');
-        `}
-        {/* <!-- Google Tag Manager --> */}
 
-        {/* <!-- End Google Tag Manager --> */}
-      </Script>
-
-      <script>
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TQQMNSWH');`}
-      </script>
     </Layout>
   );
 }
